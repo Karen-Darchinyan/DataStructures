66d4f91b477a0bcb1d2c34d3e03be3016ff85559
Karen Darchinyan <karendarchinyan99.com>
2026-04-15 19:36:59 +0400

    Add project files.

---
+++ LinkedListLib/MyLinkedListNode.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkedListLib;
+
+public class MyLinkedListNode<T>
+{
+    public T Value { get; set; }
+    public MyLinkedListNode<T> Next { get; set; }
+
+    public MyLinkedListNode(T value)
+    {
+        Value = value;
+    }
+}
